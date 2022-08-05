875d0015e1ba073fdc5d61c73fd535653c909863
NissePR <nilspetter.ruuth@gmail.com>
2022-08-05 16:01:52 +0200

    Add pro solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,11 @@
+// const leapYears = function(year) {
+//     return (year % 4 === 0 && year % 100 != 0) ? true :
+//            (year % 100 === 0 && year % 400 === 0) ? true : false;
+// };
+
 const leapYears = function(year) {
-    return (year % 4 === 0 && year % 100 != 0) ? true :
-           (year % 100 === 0 && year % 400 === 0) ? true : false;
-};
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+}
 
 // Do not edit below this line
 module.exports = leapYears;
