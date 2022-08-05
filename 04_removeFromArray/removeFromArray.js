df8b780e188252d1ad7af6a17d6cd1f4f5a68f1a
NissePR <nilspetter.ruuth@gmail.com>
2022-08-05 12:13:07 +0200

    Add the pro solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,28 @@
-const removeFromArray = function(array, ...params) {
-    for (let i = array.length; i >= 0; --i) {
-        for (let j = 0; j < params.length; ++j) {
-            if (array[i] === params[j]) {
-                array.splice(i, 1);
-            }
-        }
-    }
+// my solution ---
+// const removeFromArray = function(array, ...args) {
+//     for (let i = array.length; i >= 0; --i) {
+//         for (let j = 0; j < args.length; ++j) {
+//             if (array[i] === args[j]) {
+//                 array.splice(i, 1);
+//             }
+//         }
+//     }
 
-    return array;
-};
+//     return array;
+// };
+
+// pro solution ---
+const removeFromArray = function(...args) {
+    array = args[0];
+    newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
