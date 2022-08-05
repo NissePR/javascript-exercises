2102351549b3eae28629e9ee5afdddaa3282191c
NissePR <nilspetter.ruuth@gmail.com>
2022-08-05 12:07:26 +0200

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...params) {
+    for (let i = array.length; i >= 0; --i) {
+        for (let j = 0; j < params.length; ++j) {
+            if (array[i] === params[j]) {
+                array.splice(i, 1);
+            }
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
