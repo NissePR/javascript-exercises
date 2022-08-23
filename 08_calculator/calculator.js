a6a4ff1419a64ad0108cbbf0e8b386a7c22eed88
NissePR <nilspetter.ruuth@gmail.com>
2022-08-23 13:57:21 +0200

    Complete challenge

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+    return nums.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+    return nums.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    if (num === 0) return 1;
+
+    let sum = num;
+	for (let i = 1; i < num; i++) {
+        sum *= num - i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
