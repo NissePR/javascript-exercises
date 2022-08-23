bdab668ce02d82b96b507556fde4fefdd38c7ecd
NissePR <nilspetter.ruuth@gmail.com>
2022-08-23 14:33:12 +0200

    Complete challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = '';
+    for (let i = 0; i < string.length; ++i) {
+        const char = string.charAt(i);
+        if (char.toLowerCase() != char.toUpperCase() || char.codePointAt(0) > 127) {
+            newString += char.toLowerCase();
+        }
+    }
 
+    const reversed = newString.split('').reverse().join('');    
+    return newString === reversed;
 };
 
 // Do not edit below this line
