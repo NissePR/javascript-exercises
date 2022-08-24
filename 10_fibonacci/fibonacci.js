896fcc687e95c3b759d299fa2309309d646f2a86
NissePR <nilspetter.ruuth@gmail.com>
2022-08-24 10:46:57 +0200

    Complete challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return 'OOPS';
+    
+    let fNum1 = 0;
+    let fNum2 = 0;
+    let fNum3 = 0;
+    for (let i = 0; i < num; ++i) {
+        fNum3 = fNum1 + fNum2;
+        if (i === 0) fNum3++;
 
+        fNum1 = fNum2;
+        fNum2 = fNum3;
+    }
+
+    return fNum3;
 };
 
 // Do not edit below this line
