c877ec4edbcf63da0b8074f872806dfb61f28bfc
NissePR <nilspetter.ruuth@gmail.com>
2022-08-24 11:20:45 +0200

    Complete challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const oldest = array.reduce((oldest, current) => {
+        if (oldest.yearOfDeath == undefined) {
+            oldest.yearOfDeath = (new Date().getFullYear());
+        }
 
+        if (current) {
+            const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+            const currentAge = current.yearOfDeath - current.yearOfBirth;
+
+            if (oldestAge < currentAge) oldest = current;
+        }
+
+        return oldest;
+    })
+
+    return oldest;
 };
 
 // Do not edit below this line
